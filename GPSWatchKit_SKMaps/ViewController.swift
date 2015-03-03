97828e611f8cf1c29de474efec3c3035604510b7
Jaromir Siska <siskaj@me.com>
2015-03-03 07:22:23 +0100

    Initial Commit

---
+++ GPSWatchKit_SKMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GPSWatchKit_SKMaps
+//
+//  Created by Jaromir on 03.03.15.
+//  Copyright (c) 2015 Baltoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
